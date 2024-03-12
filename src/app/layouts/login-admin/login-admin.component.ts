a6b215c5c57dbef802cbc285c8dae941f290c410
shestermonki <faustinoludwin@gmail.com>
2024-03-12 23:37:18 +0100

    admin services

--- src/app/layouts/login-admin/login-admin.component.ts
+++ src/app/layouts/login-admin/login-admin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './login-admin.component.html',
   styleUrl: './login-admin.component.css'
 })
-export default class LoginAdminComponent {
+export default class LoginAdminComponent implements OnInit {
+  public user : any = {};
+  public usuario : any = {};
+  public token : any = '';
 
+
+  constructor(
+    private _adminService:AdminService,
+    private _router: Router
+  ) {
+    this.token = this._adminService.getToken();
+  }
+
+
+  ngOnInit(): void {
+
+
+    if(this.token){
+      this._router.navigate(['/']);
+    }else{
+      //MANTENER EN EL COMPONENTE
+    }
+
+  }
 }
