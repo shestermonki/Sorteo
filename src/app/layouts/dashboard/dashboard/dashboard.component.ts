b864dbe6784442044552ba8487ee66bec541dfd3
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2024-03-12 01:27:41 -0400

    Prueba con boton en el dashboard para autenticar

---
+++ src/app/layouts/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DashboardComponent {
+
+  private authService = inject( ApiService );
+  private router = inject( Router );
+
+  constructor(){
+    const baseUrl = 'https://discord.com/oauth2/authorize';
+    const redirectUri = 'http://localhost:3001/authorize-acount/login';
+    const params = `?client_id=1216790330196627477&response_type=code&redirect_uri=${redirectUri}&scope=identify`;
+    const urlRedirectAuthDc = `${baseUrl}${params}`;
+    console.log(urlRedirectAuthDc);
+  }
+
+  redirect(){
+    window.open('https://discord.com/oauth2/authorize?client_id=1216790330196627477&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauthorize-acount%2Flogin&scope=identify')
+  }
+
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class ApiService {
+
+  private baseUrl: string = 'http://localhost:3001';
+
+  private http = inject( HttpClient );
+
+  auth(){
+    return this.http.get(`${this.baseUrl}/authorize-acount/authorize`);
+  }
+
+}
