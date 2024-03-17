9a41df9f6c5907eff0f9b0a3854481ddd641e38f
shestermonki <faustinoludwin@gmail.com>
2024-03-17 20:01:12 +0100

    new component registrar admin y edit admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AdminGuard } from './guards/admin.guard';
 import EditUsuarioComponent from './pages/admin/edit-usuario/edit-usuario.component';
 import GaleriaProductoComponent from './pages/admin/galeria-sorteo/galeria-sorteo.component';
 import NewSorteoAdminComponent from './pages/admin/new-edit-sorteo-admin/new-sorteo-admin.component';
+import NewEditAdminComponent from './pages/admin/new-edit-admin/new-edit-admin.component';
 
 export const routes: Routes = [
   {
@@ -11,23 +12,25 @@ export const routes: Routes = [
   },
   {
     path: 'admin',
-    loadComponent: ()=> import('./layouts/layoutAdmin/layoutAdmin.component'),
+    loadComponent: () => import('./layouts/layoutAdmin/layoutAdmin.component'),
     children: [
       {
         path: 'dashboard', canActivate: [AdminGuard],
         loadComponent: () => import('./pages/admin/dashboard/dashboard.component'),
-      },{
+      }, {
         path: 'panel', children: [
           { path: 'usuarios/:id', component: EditUsuarioComponent, canActivate: [AdminGuard] },
           { path: 'sorteos/galeria/:id', component: GaleriaProductoComponent, canActivate: [AdminGuard] },
-          { path: 'newsorteo', component: NewSorteoAdminComponent, canActivate: [AdminGuard]},
-          { path: 'editsorteo/:id', component: NewSorteoAdminComponent, canActivate: [AdminGuard]},
+          { path: 'newsorteo', component: NewSorteoAdminComponent, canActivate: [AdminGuard] },
+          { path: 'editsorteo/:id', component: NewSorteoAdminComponent, canActivate: [AdminGuard] },
+          { path: 'newadmin', component: NewEditAdminComponent, canActivate: [AdminGuard] },
+          { path: 'editadmin/:id', component: NewEditAdminComponent, canActivate: [AdminGuard] },
           { path: '', redirectTo: 'newsorteo', pathMatch: 'full' }
         ]
       },
       { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
     ]
-  },{
+  }, {
     path: 'login-usuario',
     loadComponent: () => import('./layouts/login-usuario/login-usuario.component'),
   }, {
@@ -36,15 +39,15 @@ export const routes: Routes = [
     children: [
       {
         path: 'list-sorteos',
-        loadComponent: ()=> import('./pages/user/sorteo/listSorteo.component'),
-      },{
+        loadComponent: () => import('./pages/user/sorteo/listSorteo.component'),
+      }, {
         path: 'detail-sorteo/:id',
-        loadComponent: ()=> import('./pages/user/detailSorteo/detailSorteo.component'),
-      },{
+        loadComponent: () => import('./pages/user/detailSorteo/detailSorteo.component'),
+      }, {
         path: '',
         redirectTo: 'list-sorteos',
         pathMatch: 'full'
-      },{
+      }, {
         path: '**',
         redirectTo: 'list-sorteos',
       }
---
+++ src/app/interfaces/admin/response-admins.interface.ts
@@ -0,0 +1,10 @@
+export interface Admins {
+  username: string;
+  email: string;
+  password: string;
+  rol: string;
+  perfil: string;
+  sorteosAdministrados: number;
+  fechaRegistro: Date;
+  _id: string;
+}
